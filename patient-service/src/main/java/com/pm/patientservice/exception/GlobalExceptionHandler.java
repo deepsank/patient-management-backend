8fe7643e3f9ab6e5dcd055591ec02c29d5cb4809
Deepak Kumar Upadhyay <deepakkumarupadhyay63@gmail.com>
2025-04-13 17:23:25 +0530

    Added Bussiness Logic validation for email address, allowing only unique Email IDs,added a custom class for the duplicate Email ID validation and used the slfj logger to log the exceptions in the logs.

--- patient-service/src/main/java/com/pm/patientservice/exception/GlobalExceptionHandler.java
+++ patient-service/src/main/java/com/pm/patientservice/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.pm.patientservice.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,11 +13,22 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException err) {
         Map<String, String> errors = new HashMap<>();
         err.getBindingResult().getFieldErrors().forEach(
                 fieldError -> errors.put(fieldError.getField(), fieldError.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<Map<String,String>> handleEmailAlreadyExistsException(EmailAlreadyExistsException err){
+
+        log.warn("Email address already registered {}", err.toString());
+        Map<String,String> errors = new HashMap<>();
+        errors.put("message", "Email address already registered");
         return ResponseEntity.badRequest().body(errors);
     }
 
--- patient-service/src/main/java/com/pm/patientservice/repository/PatientRepository.java
+++ patient-service/src/main/java/com/pm/patientservice/repository/PatientRepository.java
@@ -1,6 +1,8 @@
 package com.pm.patientservice.repository;
 
 import com.pm.patientservice.model.PatientModel;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import java.util.UUID;
 
 @Repository
 public interface PatientRepository  extends JpaRepository<PatientModel, UUID> {
+
+    boolean existsByEmail(String email);
 }
