cd93703b451e3af88e2a3a669fd2a7bec156c228
Deepak Kumar Upadhyay <deepakkumarupadhyay63@gmail.com>
2025-04-13 18:49:36 +0530

    Added the delete patient endpoint

--- patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -40,4 +40,10 @@ public class PatientController {
         return ResponseEntity.ok().body(modifiedPatientResponseDTO);
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePatient(@PathVariable UUID id){
+        patientService.deletePatient(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
